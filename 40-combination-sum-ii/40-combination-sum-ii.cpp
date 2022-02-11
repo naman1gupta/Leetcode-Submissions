a5e454e3e0c39e212f279a618f27ee0319de6fa9
naman1gupta <72181247+naman1gupta@users.noreply.github.com>
2022-02-11 17:05:23 +0530

    Time: 9 ms (50.02%), Space: 10.8 MB (38.55%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,46 @@
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n";
+#define PI 3.1415926535
+#define SORT(v)  sort(v.begin(), v.end())
+#define SORTR(v) sort(v.rbegin(), v.rend())
+#define MAX(v) *max_element(v.begin(), v.end())
+#define MIN(v) *min_element(v.begin(), v.end())
+
+typedef long long int ll;
+
+const ll N = 3e5 + 10;
+const ll mod = 1e9 + 7;
+const ll MAXX = 1e6 + 5;
+const ll nn=1e5 + 1;
+
+
+
+class Solution {
+     void combinationSum2(vector<int> &candidates, int target, vector<vector<int> > &res, vector<int> &combination, int begin) {
+        if (!target) {
+            res.push_back(combination);
+            return;
+        }
+        for (int i = begin; i != candidates.size() && target >= candidates[i]; ++i) {
+            if(i&&candidates[i]==candidates[i-1]&&i>begin) {
+                continue;
+            }
+            combination.push_back(candidates[i]);
+            combinationSum2(candidates, target - candidates[i], res, combination, i+1);
+            combination.pop_back();
+        }
+    }
+    
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+         FAST;
+        // ll n=candidates.size();
+        vector<vector<int>>res;
+        vector<int>combination;
+        SORT(candidates);
+        combinationSum2(candidates, target, res, combination, 0);
+        return res;
+    }
+};
