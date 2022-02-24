b29f1105792c19ea3520d045f29e839989502311
naman1gupta <72181247+naman1gupta@users.noreply.github.com>
2022-02-24 16:57:08 +0530

    Time: 56 ms (49.64%), Space: 28.5 MB (42.57%) - LeetHub

---
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
@@ -0,0 +1,32 @@
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n";
+#define PI 3.1415926535
+#define SORT(v)  sort(v.begin(), v.end())
+#define SORTR(v) sort(v.rbegin(), v.rend())
+#define MAX(v) *max_element(v.begin(), v.end())
+#define MIN(v) *min_element(v.begin(), v.end())
+#define all(v) (v).begin(),(v).end()
+#define forn(i,n) for(int i=0;i<(n);i++)
+typedef long long int ll;
+
+const ll N = 3e5 + 10;
+const ll mod = 1e7 + 7;
+const ll MAXX = 1e6 + 5;
+const ll nn=1e5 + 1;
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        FAST;
+        sort(all(nums));
+        ll sum=0;
+        ll n=nums.size();
+        for(ll i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        return (sum - nums[0]*n);
+        
+        
+    }
+};
