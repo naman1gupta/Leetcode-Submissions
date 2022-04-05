14fbb26398b32a6f9e8e0d547dce151924e89b7a
naman1gupta <72181247+naman1gupta@users.noreply.github.com>
2022-04-05 22:19:19 +0530

    Time: 165 ms (17.27%), Space: 59 MB (81.47%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -0,0 +1,39 @@
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n";
+#define PI 3.1415926535
+#define SORT(v)  sort(v.begin(), v.end())
+#define SORTR(v) sort(v.rbegin(), v.rend())
+#define MAX(v) *max_element(v.begin(), v.end())
+#define MIN(v) *min_element(v.begin(), v.end())
+#define all(v) (v).begin(),(v).end()
+#define forn(i,n) for(int i=0;i<(n);i++)
+typedef long long int ll;
+
+const ll N = 3e5 + 10;
+const ll mod = 1e7 + 7;
+const ll MAXX = 1e6 + 5;
+const ll nn=1e5 + 1;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int maxi = 0;
+        while(left < right){
+            int w = right - left;
+            int h = min(height[left], height[right]);
+            int area = h * w;
+            maxi = max(maxi, area);
+            if(height[left] < height[right]) left++;
+            else if(height[left] > height[right]) right--;
+            else {
+                left++;
+                right--;
+            }
+        }
+        return maxi;
+    }
+};
