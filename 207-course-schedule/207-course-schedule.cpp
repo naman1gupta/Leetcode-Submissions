852e17fbdd5acbf7523e379e6606267ed0d36061
naman1gupta <72181247+naman1gupta@users.noreply.github.com>
2022-07-07 11:30:34 +0530

    Time: 47 ms (22.37%), Space: 14.7 MB (26.35%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,47 @@
+// class Solution {
+// public:
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+//     }
+// };
+class Solution {
+    private:
+    typedef vector<vector<int>> graph;
+    
+    graph buildGraph(int numCourses, vector<vector<int>>& prerequisites) {
+        graph g(numCourses);
+        for (auto p : prerequisites) {
+            g[p[1]].push_back(p[0]);
+        }
+        return g;
+    }
+    
+    bool acyclic(graph& g, vector<bool>& todo, vector<bool>& done, int node) {
+        if (todo[node]) {
+            return false;
+        }
+        if (done[node]) {
+            return true;
+        }
+        todo[node] = done[node] = true;
+        for (int v : g[node]) {
+            if (!acyclic(g, todo, done, v)) {
+                return false;
+            }
+        }
+        todo[node] = false;
+        return true;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        graph g = buildGraph(numCourses, prerequisites);
+        vector<bool> todo(numCourses, false), done(numCourses, false);
+        for (int i = 0; i < numCourses; i++) {
+            if (!done[i] && !acyclic(g, todo, done, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+};
