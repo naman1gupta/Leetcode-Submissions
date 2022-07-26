d38d1d737092a8b28e83d866a6096f1374d28c4f
naman1gupta <72181247+naman1gupta@users.noreply.github.com>
2022-07-26 11:24:52 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (77.19%) - LeetHub

--- 394-decode-string/394-decode-string.cpp
+++ 394-decode-string/394-decode-string.cpp
@@ -1,37 +1,30 @@
-
 class Solution {
-string repeat(string str, int times) {
-        string res = "";
-        for (int i=0; i<times; i++)
-            res += str;
+public:
+    string decodeString(const string& s, int& i) {
+        string res;
+        
+        while (i < s.length() && s[i] != ']') {
+            if (!isdigit(s[i]))
+                res += s[i++];
+            else {
+                int n = 0;
+                while (i < s.length() && isdigit(s[i]))
+                    n = n * 10 + s[i++] - '0';
+                    
+                i++; // '['
+                string t = decodeString(s, i);
+                i++; // ']'
+                
+                while (n-- > 0)
+                    res += t;
+            }
+        }
+        
         return res;
     }
-    public:
-    
-    string decodeString(string s) {
-        int i=0;
-        while (i < s.size()) {
-            if (s[i] != ']') {
-                i++;
-                continue;
-            }
 
-            int j = i;
-            while (s[j] != '['){
-                j--;
-            }
-            string letters_to_repeat = s.substr(j+1, i-j-1);
-            int k = j;
-            j--;
-            while ((j > 0) &&(isdigit(s[j])))
-                j--;
-            
-            if (j != 0) j++;
-            int times_to_repeat = stoi(s.substr(j, k-j));
-            
-            s.replace(j, i-j+1, repeat(letters_to_repeat, times_to_repeat));
-            i = j+letters_to_repeat.size()*times_to_repeat;
-        }
-        return s;
+    string decodeString(string s) {
+        int i = 0;
+        return decodeString(s, i);
     }
 };
