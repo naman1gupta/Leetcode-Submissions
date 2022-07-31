97e239e5604315f166243b3c745f6cd65aef914e
naman1gupta <72181247+naman1gupta@users.noreply.github.com>
2022-07-31 10:44:52 +0530

    Time: 360 ms (100.00%), Space: 135.1 MB (100.00%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int maxLength = -1;
+
+    void getcycle(vector<int> &edges,int si,vector<bool>& visit,int size,vector<int>& store){
+        if(si == -1)return ;
+        if(visit[si]){
+            int count = -1;
+            for(int i =0;i<store.size();i++){
+                if(store[i]==si){
+                    count = i;
+                    break;
+                } }
+            
+            if(count==-1)return;
+            maxLength = max(maxLength,size-count);  // size-count gives the proper size of cycle
+            return ;
+        }
+        
+        visit[si] = true;
+        store.push_back(si);
+        getcycle(edges,edges[si],visit,size+1,store);
+        
+        return ;
+        
+    }
+    
+    
+    
+    int longestCycle(vector<int>& edges) {
+        
+        vector<bool> visit(edges.size(),0);
+        for(int i =0;i<edges.size();i++){
+            
+            if(visit[i])continue;
+            vector<int> store;
+            getcycle(edges,i,visit,0,store);
+     
+        }
+        
+        return maxLength;
+        
+    }
+};
